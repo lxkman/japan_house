8405fd4265e8b30382b639696676aa4c21d18a0e
yanfei <1539145165@qq.com>
2018-04-19 11:29:39 +0800

    yf

--- app/src/main/java/com/example/administrator/japanhouse/fragment/mine/SingUpDetailsActivity.java
+++ app/src/main/java/com/example/administrator/japanhouse/fragment/mine/SingUpDetailsActivity.java
@@ -1,15 +1,29 @@
 package com.example.administrator.japanhouse.fragment.mine;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.administrator.japanhouse.R;
 import com.example.administrator.japanhouse.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SingUpDetailsActivity extends BaseActivity {
+
+    @BindView(R.id.back_img)
+    ImageView backImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up_details);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.back_img)
+    public void onClick() {
+        finish();
     }
 }

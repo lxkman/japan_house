d2f94dd03ee43c1c8c63797315c34f6422820663
wl <wangle1109@outlook.com>
2018-05-23 11:34:58 +0800

    智能买房

--- app/src/main/java/com/example/administrator/japanhouse/fragment/home/ui/activity/ZhinengActivity.java
+++ app/src/main/java/com/example/administrator/japanhouse/fragment/home/ui/activity/ZhinengActivity.java
@@ -8,22 +8,28 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.administrator.japanhouse.R;
 import com.example.administrator.japanhouse.base.BaseActivity;
 import com.example.administrator.japanhouse.bean.EventBean;
+import com.example.administrator.japanhouse.im.ChatMsgAdapter;
+import com.example.administrator.japanhouse.model.ZNHouseBean;
+import com.example.administrator.japanhouse.presenter.ZNBuyHousePresenter;
+import com.example.administrator.japanhouse.utils.CacheUtils;
 import com.example.administrator.japanhouse.utils.Constants;
-import com.example.administrator.japanhouse.utils.MyUtils;
-import com.example.administrator.japanhouse.utils.TUtils;
 import com.example.administrator.japanhouse.view.FluidLayout;
+import com.lzy.okgo.model.Response;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class ZhinengActivity extends BaseActivity implements View.OnClickListener {
+public class ZhinengActivity extends BaseActivity implements View.OnClickListener, ZNBuyHousePresenter.BuyHouseCallBack, ChatMsgAdapter.OnBackgroundClickListener {
 
     private ImageView img_beak;
     private ImageView xinxi;
@@ -35,6 +41,19 @@ public class ZhinengActivity extends BaseActivity implements View.OnClickListene
     private int windowWidth;
     private LinearLayout linearLayout;
 
+    private ZNBuyHousePresenter presenter;
+
+    private List<String> issueList = new ArrayList<>();
+    private List<String> answerList = new ArrayList<>();
+
+    private ListView listView;
+    private ChatMsgAdapter msgAdapter;
+
+    private List<String> adapterList = new ArrayList<>();
+    private List<Boolean> booleenList = new ArrayList<>();
+    private List<Boolean> timeList = new ArrayList<>();
+    private List<String> tList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,36 +64,34 @@ public class ZhinengActivity extends BaseActivity implements View.OnClickListene
         wm.getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         float density = dm.density;
-        windowWidth = (int) (width/density);
+        windowWidth = (int) (width / density);
 
         tvleft = (TextView) findViewById(R.id.act_zn_cainixiagnwen);
         linearLayout = (LinearLayout) findViewById(R.id.act_zn_liner);
+        listView = (ListView) findViewById(R.id.act_zn_listView);
+
+        long longTime = 0;
+        CacheUtils.put(Constants.SYSTEM_TIME, longTime);
 
         initView();
+
     }
 
     private void initView() {
-        final List<String> list = new ArrayList<>();
-        list.add(getString(R.string.zbmf));
-        list.add(getString(R.string.daikuan));
-        list.add(getString(R.string.baike));
-        list.add(getString(R.string.zbmf));
-        list.add(getString(R.string.daikuan));
-        list.add(getString(R.string.baike));
-        list.add(getString(R.string.zbmf));
-        list.add(getString(R.string.daikuan));
-        list.add(getString(R.string.baike));
-        list.add(getString(R.string.zbmf));
-        list.add(getString(R.string.daikuan));
-        list.add(getString(R.string.baike));
 
         img_beak = (ImageView) findViewById(R.id.img_beak);
         xinxi = (ImageView) findViewById(R.id.xinxi);
         fluidLayout = (FluidLayout) findViewById(R.id.act_zn_FluidLayout);
         more = (TextView) findViewById(R.id.act_zn_more);
         flowLineLayout = (LinearLayout) findViewById(R.id.act_zn_lineFlow);
+        more.setVisibility(View.GONE);
 
-        init(list);
+        presenter = new ZNBuyHousePresenter(this, this);
+        presenter.getZNBuyHouseList();
+        init(issueList);
+
+        msgAdapter = new ChatMsgAdapter(this, adapterList, this, booleenList, timeList, tList);
+        listView.setAdapter(msgAdapter);
 
         img_beak.setOnClickListener(this);
         xinxi.setOnClickListener(this);
@@ -85,16 +102,17 @@ public class ZhinengActivity extends BaseActivity implements View.OnClickListene
                 more.setVisibility(View.GONE);
                 flowLineLayout.setVisibility(View.GONE);
                 fluidLayout.setVisibility(View.VISIBLE);
-                initHot(list);
+                initHot(issueList);
             }
         });
-        findViewById(R.id.act_zn_relative).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.act_zn_clickBg).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 more.setVisibility(View.VISIBLE);
                 flowLineLayout.setVisibility(View.VISIBLE);
                 fluidLayout.setVisibility(View.GONE);
-                init(list);
+                init(issueList);
             }
         });
     }
@@ -127,7 +145,10 @@ public class ZhinengActivity extends BaseActivity implements View.OnClickListene
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    TUtils.showShort(mContext, "点击了---" + hotNameList.get(finalI));
+                    booleenList.add(true);
+                    adapterList.add(hotNameList.get(finalI));
+                    setTimeShow();
+                    answerIssue(finalI);
                 }
             });
         }
@@ -153,21 +174,74 @@ public class ZhinengActivity extends BaseActivity implements View.OnClickListene
             tv.setLayoutParams(params);
             tv.measure(mWidth, mHeight);
 
-            if (width + tv.getMeasuredWidth() > windowWidth - tvLeft.width - tvRight.width - tvLeft.leftMargin - tvRight.rightMargin) {
+            width += tv.getMeasuredWidth();
+
+            if (width > windowWidth - tvLeft.width - tvRight.width - tvLeft.leftMargin - tvRight.rightMargin) {
                 tv.setVisibility(View.GONE);
                 break;
             }
-            width += tv.getMeasuredWidth();
+
             flowLineLayout.addView(tv);
 
             final int finalI = i;
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    TUtils.showShort(mContext, "点击了---" + hotNameList.get(finalI));
+                    booleenList.add(true);
+                    adapterList.add(hotNameList.get(finalI));
+                    setTimeShow();
+                    answerIssue(finalI);
                 }
             });
 
         }
+    }
+
+    private void answerIssue(int position){
+        booleenList.add(false);
+        adapterList.add(answerList.get(position));
+        msgAdapter.notifyDataSetChanged();
+        setTimeShow();
+    }
+
+    @Override
+    public void getZNBuyHouseList(Response<ZNHouseBean> response) {
+        if (response.body().getDatas() == null && response.body().getDatas().size() == 0)
+            return;
+
+        List<ZNHouseBean.DatasBean> data = response.body().getDatas();
+        for (int i = 0; i < data.size(); i++) {
+            issueList.add(data.get(i).getTheKeyword());
+            answerList.add(data.get(i).getAnswer());
+        }
+        more.setVisibility(View.VISIBLE);
+        init(issueList);
+    }
+
+    @Override
+    public void onClickListener() {
+        more.setVisibility(View.VISIBLE);
+        flowLineLayout.setVisibility(View.VISIBLE);
+        fluidLayout.setVisibility(View.GONE);
+        init(issueList);
+    }
+
+    private void setTimeShow(){
+
+        long laterTime = System.currentTimeMillis();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String timeString = sdf.format(new Date(laterTime));
+        long beforeTime = CacheUtils.get(Constants.SYSTEM_TIME);
+        if (beforeTime == 0) {
+            timeList.add(true);
+        } else {
+            if (laterTime - beforeTime > 3 * 60 * 1000) {
+                timeList.add(true);
+            } else {
+                timeList.add(false);
+            }
+        }
+        tList.add(timeString);
+        CacheUtils.put(Constants.SYSTEM_TIME, laterTime);
     }
 }
